b8eecf57a5d63bfe577f27fd7ae40262cb3eb9f5
Shravya9506 <65528425+Shravya9506@users.noreply.github.com>
2021-10-07 16:37:13 -0500

    Delete ScatterPlot1.R

--- Plots/ScatterPlot1.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-hfsData <- read.csv("HFS Service Data.csv")
-ggplot(hfsData, aes(gender, gender_identity)) + 
-  ggtitle("Data Anomalies for Gender and Gender identity") + 
-  xlab("Gender") + 
-  ylab("Gender Identity") +
-  geom_point()
